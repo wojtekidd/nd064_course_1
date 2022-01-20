eb43313e509f74d0bf19cc8e10a404979ae52622
Wojtek Cichon <wojciech.cichon@gmail.com>
2022-01-20 10:45:35 +0100

    Miniscule changes

--- exercises/python-helloworld/app.py
+++ exercises/python-helloworld/app.py
@@ -1,9 +1,48 @@
+from email.mime import application
+from urllib import response
 from flask import Flask
+from flask import json
+import logging
+
 app = Flask(__name__)
+
+@app.route("/status")
+def status():
+    response = app.response_class(
+        response=json.dumps({"result":"OK HEALTHY"}),
+        status=200,
+        mimetype="application/json"
+    )
+
+    return response
+
+# logging
+    app.logger.info("Status request siccessfull")
+    return response
+
+@app.route("/metrics")
+def metrics():
+    response = app.response_class(
+        response=json.dumps({"status":"success", "code":0,"data":{"UserCount":140,"UserCountActive":23}}),
+        status=200,
+        mimetype="application/json"
+    )
+
+    return response
+
+# logging 
+    app.logger.info("Metrics request succesfull")
+    return response
 
 @app.route("/")
 def hello():
+    app.logger.info("Main request successfull")
+
     return "Hello World!"
 
 if __name__ == "__main__":
+
+    #stream logs to app.log file
+    logging.basicConfig(filename="app.log", level=logging.DEBUG)
+
     app.run(host='0.0.0.0')
